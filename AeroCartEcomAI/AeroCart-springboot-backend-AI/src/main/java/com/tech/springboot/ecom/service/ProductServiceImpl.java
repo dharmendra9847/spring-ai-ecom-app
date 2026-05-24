3295b4abba7db42948b8d21c6d6ba892b0e71891
Dharmendra Kumar <dharmendra9847@gmail.com>
2026-05-25 01:10:07 +0530

    feat(service): implement product vector embedding logic for semantic search

--- AeroCartEcomAI/AeroCart-springboot-backend-AI/src/main/java/com/tech/springboot/ecom/service/ProductServiceImpl.java
+++ AeroCartEcomAI/AeroCart-springboot-backend-AI/src/main/java/com/tech/springboot/ecom/service/ProductServiceImpl.java
@@ -2,18 +2,28 @@ package com.tech.springboot.ecom.service;
 
 import com.tech.springboot.ecom.model.entity.Product;
 import com.tech.springboot.ecom.repo.ProductRepo;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    private final ProductRepo productRepo;
+    private final VectorStore vectorStore;
+
     @Autowired
-    private ProductRepo productRepo;
+    public ProductServiceImpl(ProductRepo productRepo, VectorStore vectorStore) {
+        this.productRepo = productRepo;
+        this.vectorStore = vectorStore;
+    }
 
     @Override
     public List<Product> getAllProducts() {
@@ -31,7 +41,39 @@ public class ProductServiceImpl implements ProductService {
         product.setImageType(imageFile.getContentType());
         product.setImageData(imageFile.getBytes());
 
-        return productRepo.save(product);
+        Product savedProduct = productRepo.save(product);
+
+        String content = String.format(
+                """
+                Product Name: %s
+                Description: %s
+                Brand: %s
+                Category: %s
+                Price: %.2f
+                Release Date: %s
+                Available: %s
+                Stock: %d
+                """,
+                savedProduct.getName(),
+                savedProduct.getDescription(),
+                savedProduct.getBrand(),
+                savedProduct.getCategory(),
+                savedProduct.getPrice(),
+                savedProduct.getReleaseDate(),
+                savedProduct.isProductAvailable(),
+                savedProduct.getStockQuantity()
+
+        );
+
+        Document document = new Document(
+                UUID.randomUUID().toString(),
+                content,
+                Map.of("productId", String.valueOf(savedProduct.getId()))
+        );
+
+        vectorStore.add(List.of(document));
+
+        return savedProduct;
     }
 
     @Override
